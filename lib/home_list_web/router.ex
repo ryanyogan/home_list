0d39b1011270a52adcec726afa96257c7d510db7
ryan <ryanyogan@gmail.com>
2021-09-17 12:45:12 -0500

    basic absinthe setup

--- lib/home_list_web/router.ex
+++ lib/home_list_web/router.ex
@@ -19,18 +19,13 @@ defmodule HomeListWeb.Router do
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", HomeListWeb do
-  #   pipe_through :api
-  # end
+  scope "/api" do
+    pipe_through :api
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
+    get "/", Absinthe.Plug.GraphiQL, schema: HomeListWeb.Api.Schema, interface: :playground
+    post "/", Absinthe.Plug, schema: HomeListWeb.Api.Schema
+  end
+
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
--- mix.exs
+++ mix.exs
@@ -14,9 +14,6 @@ defmodule HomeList.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {HomeList.Application, []},
@@ -24,13 +21,9 @@ defmodule HomeList.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.5.3"},
@@ -44,16 +37,12 @@ defmodule HomeList.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.6.5"},
+      {:absinthe_plug, "~> 1.5.8"}
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
